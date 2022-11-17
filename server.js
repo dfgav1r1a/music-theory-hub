ff86781306ab895898109c0e8cc41070a3095afd
Daniel Gaviria <danielfgaviria@gmail.com>
2022-11-17 16:55:01 -0500

    created public directory and moved files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-exoress.use(express.static('public'));
+app.use(express.static('public'));
 
 app.listen(port , () => {
     console.log(`Application running in ${port}`);
